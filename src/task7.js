63888420558a3211f1a3025ae12c124b632093a9
Christian <al222011226@gmail.com>
2025-09-03 16:36:37 -0600

    Solución

--- src/task7.js
+++ src/task7.js
@@ -6,6 +6,22 @@ const arr = [
   
   // Type your code below this line!
   
-  
+// ¿Pueden agregar un solo número a una fila existente?
+arr[0].push(10);
+console.log("Se agrega el numero diez al primer arreglo: ",arr[0]);
+
+// ¿Pueden agregar una fila completa de números?
+const newRow = [30,31,32,33,34,35,36,37,38,39];
+arr.push(newRow);
+console.log("Se agrega esta fila nueva: ",arr);
+
+// ¿Pueden eliminar un solo número de una fila específica?
+arr[1].splice(5,1); // Elimina el número 15 de la segunda fila
+console.log("Se elimina el número 15 de la segunda fila:", arr[1]);
+
+// ¿Pueden invertir una de las filas sin afectar a las demás?
+arr[1].reverse();
+console.log("Se invierte el arreglo 2: ", arr[0], arr[1],  arr[2]);
+
   
   // Type your code above this line!
